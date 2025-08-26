8d8a63e1f805af17e42e2e998693b01781bcd5f6
maula iqbal <rifqimaula@gmail.com>
2025-08-26 09:00:45 +0700

    'tawk to'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,19 @@
 
 
 </head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/68ad13e9fcd547192ddee611/1j3i1bifn';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 <body>
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-info">
@@ -166,7 +179,10 @@
             crossorigin="anonymous"></script>
 
     <!-- Bootstrap 4 JS Bundle (includes Popper) -->
+    <!--Start of Tawk.to Script-->
 
+<!--End of Tawk.to Script-->
     @stack('scripts')
+
 </body>
 </html>
