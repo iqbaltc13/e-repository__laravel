5c6e73791eb7a54dbf1bcd2c1feeec2f4566d934
iqbaltc13 <rifqimaula@gmail.com>
2025-08-27 22:11:51 +0700

    'JounralAuthors'

--- app/Http/Controllers/JournalAuthorController.php
+++ app/Http/Controllers/JournalAuthorController.php
@@ -16,15 +16,15 @@ class JournalAuthorController extends Controller
 
     public function index(Journal $journal)
     {
-        $this->authorize('view', $journal);
+        //$this->authorize('view', $journal);
 
-        $authors = $journal->journalAuthors()->orderBy('order')->get();
+        $authors = $journal->coAuthors()->orderBy('order')->get();
         return view('journal-authors.index', compact('journal', 'authors'));
     }
 
     public function create(Journal $journal)
     {
-        $this->authorize('update', $journal);
+        //$this->authorize('update', $journal);
 
         return view('journal-authors.create', compact('journal'));
     }
@@ -42,27 +42,27 @@ class JournalAuthorController extends Controller
             'is_corresponding' => 'boolean',
         ]);
 
-        $journal->journalAuthors()->create($request->all());
+        $journal->coAuthors()->create($request->all());
 
         return redirect()->route('journal-authors.index', $journal)->with('success', 'Author berhasil ditambahkan!');
     }
 
     public function edit(Journal $journal, JournalAuthor $journalAuthor)
     {
-        $this->authorize('update', $journal);
+        //$this->authorize('update', $journal);
 
         return view('journal-authors.edit', compact('journal', 'journalAuthor'));
     }
 
     public function update(Request $request, Journal $journal, JournalAuthor $journalAuthor)
     {
-        $this->authorize('update', $journal);
+        //$this->authorize('update', $journal);
 
         $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
             'email' => 'required|email',
-            'institution' => 'required|string|max:255',
+            'institution' => 'nullable|string|max:255',
             'order' => 'required|integer|min:1',
             'is_corresponding' => 'boolean',
         ]);
@@ -74,7 +74,7 @@ class JournalAuthorController extends Controller
 
     public function destroy(Journal $journal, JournalAuthor $journalAuthor)
     {
-        $this->authorize('update', $journal);
+        //$this->authorize('update', $journal);
 
         $journalAuthor->delete();
 
