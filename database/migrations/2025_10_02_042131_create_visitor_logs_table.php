e5ab976b4cab122bff7dade84950e8a208d95d61
maula iqbal <rifqimaula@gmail.com>
2025-10-02 13:39:14 +0700

    'Feature : Visitor Logger'

---
+++ database/migrations/2025_10_02_042131_create_visitor_logs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function __construct()
+    {
+        $this->table_name =  'visitor_logs';
+        $this->schema = Schema::connection($this->getConnection());
+    }
+
+    public function up(): void
+    {
+        $this->schema->create($this->table_name, function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string('user_id', 255)->nullable();
+            $table->ipAddress('ip')->nullable();
+            $table->text('endpoint')->nullable();
+            $table->dateTime('accessed_at')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $this->schema->dropIfExists($this->table_name);
+    }
+
+
+};
